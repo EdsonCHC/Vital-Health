550b3e5b2bb2bd506475d8536572915b34df83c0
Edson CHC <105886687+EdsonCHC@users.noreply.github.com>
2024-03-03 21:41:31 -0600

    feta: add a test html

---
+++ vh/resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="flex w-full h-screen">
+    <div class="bg-lime-300 w-2/4 grid place-items-center rounded-tr-lg rounded-br-lg">
+        <div>    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-activity-heartbeat" width="400" height="350" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M3 12h4.5l1.5 -6l4 12l2 -9l1.5 3h4.5" /></svg>
+        <h1 class="text-6xl text-white font-bold">Vital Health</h1></div>
+    </div>
+    <div class="w-2/4 p-5">
+        <ul class="w-full h-16 flex justify-center items-center">
+            <li class="inline-block ml-16 text-2xl font-semibold">Home</li>
+            <li class="inline-block ml-16 text-2xl font-semibold">Login</li>
+            <li class="inline-block ml-16 text-2xl font-semibold">Registro</li>
+            <li class="inline-block ml-16 text-2xl font-semibold">Servicios</li>
+        </ul>
+        <h1 class="text-black text-8xl uppercase italic font-bold mt-40">Esto es</h1>
+        <h1 class="text-black text-8xl uppercase italic font-bold">vital health</h1>
+        <h2 class="text-4xl">Tu salud es nuestro beneficio</h2>
+        <p class="mt-20 w-3/5 text-xl">
+        ¿Por qué es importante la salud? Existen varios beneficios de tener una vida saludable, pero el principal de ellos que podríamos nombrar es que nuestro cuerpo se libera de las diversas formas de trastornos y complicaciones y, por tanto, se obtiene una vida más larga, sin sufrir ningún tipo de dolores o malestares.
+        </p>
+        <p class="text-lg mt-56">&copy 2024 Vital Health. Todos los derechos reservados</p>
+    </div>
+</body>
+</html>
---
+++ vh/resources/views/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de sesión</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="flex w-full h-full">
+    <div class="bg-color-green w-2/4"></div>
+    <div></div>
+</body>
+</html>
