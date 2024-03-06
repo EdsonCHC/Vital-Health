14c98d823b47706eca62e919ab4ea2902b4193ee
EdsonCHC <105886687+EdsonCHC@users.noreply.github.com>
2024-03-06 14:20:19 -0600

    doing the resposive site, through mobile first method

--- vh/resources/views/app/login.blade.php
+++ vh/resources/views/app/login.blade.php
@@ -6,15 +6,15 @@
     <title>Inicio de sesión</title>
     @vite('resources/css/app.css')
 </head>
-<body class="flex w-full h-screen p-8">
-    <div class="bg-gray-100 w-1/2 h-full">
-        <h3 class="w-auto h-auto text-lime-400 font-bold relative top-0 left-0">Vital Health <3 </h3>
-        <div id="form-div">
-        <form action="">
-            <h4>Inicio de sesión</h4>
-            <Input  class="block mb-1" type="text" placeholder="Nombre de Usuario o Email="></Input>
-            <input  class="block mb-1" placeholder="Contaseña" type="password">
-        </form>
+<body class="flex flex-column  bg-white w-full h-full p-2 lg:h-screen lg:p-8 ">
+    <div class="w-full lg:w-3/5 h-full flex flex-col items-center">
+        <h3 class="w-auto h-auto text-lime-400 font-bold flex self-start">Vital Health <3 </h3>
+        <div id="form-div" class="grid place-items-center">
+            <form action="">
+                <h4>Inicio de sesión</h4>
+                <Input  class="block mb-1" type="text" placeholder="Nombre de Usuario o Email="></Input>
+                <input  class="block mb-1" placeholder="Contaseña" type="password">
+            </form>
         </div>
         <p>¿no posees cuenta aun? <a href="/registro" class="text-lime-300">Crear Cuenta</a></p>
         <div class="flex gap-1 items-center">
@@ -23,6 +23,8 @@
             <hr class="bg-gray-300 w-14">
         </div>
     </div>
-    <div class="bg-lime-400 w-1/2 rounded-xl"></div>
+    <div class="hidden lg:block bg-lime-500 w-2/5 rounded-xl p-11">
+        <div class="lg:bg-black opacity-20 w-full h-full rounded-xl"></div>
+    </div>
 </body> 
 </html>
