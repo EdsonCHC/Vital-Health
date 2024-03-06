01d2bc4205d32eb243b4d4133905bee6b5b4cae1
EdsonCHC <105886687+EdsonCHC@users.noreply.github.com>
2024-03-06 13:17:30 -0600

    feat: begining of the login page

--- vh/resources/views/app/login.blade.php
+++ vh/resources/views/app/login.blade.php
@@ -6,8 +6,23 @@
     <title>Inicio de sesión</title>
     @vite('resources/css/app.css')
 </head>
-<body class="flex w-full h-full">
-    <div class="bg-color-green w-2/4"></div>
-    <div></div>
-</body>
+<body class="flex w-full h-screen p-8">
+    <div class="bg-gray-100 w-1/2 h-full">
+        <h3 class="w-auto h-auto text-lime-400 font-bold relative top-0 left-0">Vital Health <3 </h3>
+        <div id="form-div">
+        <form action="">
+            <h4>Inicio de sesión</h4>
+            <Input  class="block mb-1" type="text" placeholder="Nombre de Usuario o Email="></Input>
+            <input  class="block mb-1" placeholder="Contaseña" type="password">
+        </form>
+        </div>
+        <p>¿no posees cuenta aun? <a href="/registro" class="text-lime-300">Crear Cuenta</a></p>
+        <div class="flex gap-1 items-center">
+            <hr class="bg-gray-300 w-14">
+            <p>Ingresa Tambien con</p>
+            <hr class="bg-gray-300 w-14">
+        </div>
+    </div>
+    <div class="bg-lime-400 w-1/2 rounded-xl"></div>
+</body> 
 </html>
--- vh/routes/web.php
+++ vh/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('app.login');
 });
+
+Route::get('/registro', function () {
+    return view('app.registro');
+});
