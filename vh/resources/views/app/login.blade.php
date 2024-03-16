6a0b1504fdc366865facd7147d7d60e4f60099d8
Edson CHC <105886687+EdsonCHC@users.noreply.github.com>
2024-03-15 23:54:13 -0600

    feat(login) made view for mobile devices

--- vh/resources/views/app/login.blade.php
+++ vh/resources/views/app/login.blade.php
@@ -3,28 +3,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
     <title>Inicio de sesión</title>
     @vite('resources/css/app.css')
 </head>
-<body class="flex flex-column  bg-white w-full h-full p-2 lg:h-screen lg:p-8 ">
-    <div class="w-full lg:w-3/5 h-full flex flex-col items-center">
-        <h3 class="w-auto h-auto text-lime-400 font-bold flex self-start">Vital Health <3 </h3>
-        <div id="form-div" class="grid place-items-center">
-            <form action="">
-                <h4>Inicio de sesión</h4>
-                <Input  class="block mb-1" type="text" placeholder="Nombre de Usuario o Email="></Input>
-                <input  class="block mb-1" placeholder="Contaseña" type="password">
+<body class="flex flex-col bg-white w-full h-screen p-2 lg:h-screen lg:p-8 ">
+    <div class="w-full h-screen flex flex-col items-center flex-1 lg:w-3/5">
+        <object data="" type=""></object><!--LOGO-->
+        <div id="form-div" class="grid place-items-center w-full">
+            <h4 class="text-2xl font-bold mb-8">Inicio de sesión</h4>
+            <form action="" class="text-center w-full flex flex-col items-center">
+                <Input  class="block h-10 p-2 outline outline-1 outline-vh-green w-4/5 mb-2" type="text" placeholder="Nombre de Usuario o Email"></Input>
+                <input  class="block h-10 p-2 outline outline-1 outline-vh-green w-4/5 mb-10" placeholder="Contraseña" type="password">
+                <button class="w-4/5 h-10 font-bold text-1xl shadow-xl mb-10 shadow-vh-green opacity-25">
+                Ingresar</button>
             </form>
         </div>
-        <p>¿no posees cuenta aun? <a href="/registro" class="text-lime-300">Crear Cuenta</a></p>
+        <p class="mb-8">¿No posees cuenta aun? <a href="/registro" class="text-vh-green font-bold">Crear Cuenta</a></p>
         <div class="flex gap-1 items-center">
-            <hr class="bg-gray-300 w-14">
-            <p>Ingresa Tambien con</p>
-            <hr class="bg-gray-300 w-14">
+            <hr class="border border-vh-green opacity-25 w-14">
+            <p>Ingresa También con</p>
+            <hr class="border border-vh-green opacity-25 w-14">
         </div>
+        <div id="my-signin2"></div>
+        <p id="si" class="flex grow items-end text-vh-green text-center">© 2024 Vital Health Todos los derechos reservados</p>
     </div>
     <div class="hidden lg:block bg-lime-500 w-2/5 rounded-xl p-11">
         <div class="lg:bg-black opacity-20 w-full h-full rounded-xl"></div>
     </div>
+    <!--COSO DE GOOGLE-->
+    <script> 
+    function onSuccess(googleUser) {
+      console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    }
+    function onFailure(error) {
+      console.log(error);
+    }
+    function renderButton() {
+      gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 250,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'white',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure,
+      });
+    }
+  </script>
+    <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script> <!--API DE GOOGLE-->
 </body> 
 </html>
