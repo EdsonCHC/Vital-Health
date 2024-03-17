bab1e41ef1e8c0e27d9545b8f0adf1901bb921ce
Edson CHC <105886687+EdsonCHC@users.noreply.github.com>
2024-03-16 21:46:37 -0600

    fix(index): change the login button to a link

--- vh/resources/views/app/index.blade.php
+++ vh/resources/views/app/index.blade.php
@@ -3,31 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prueba</title>
+    <title>Inicio</title>
     @vite('resources/css/app.css')
-    @vite(['resources/js/app.js'])
+    <!-- @vite(['resources/js/app.js']) not used now  -->
 </head>
-<body class="flex w-full h-screen">
-    <div class="bg-lime-600 w-2/4 grid place-items-center rounded-tr-lg rounded-br-lg">
-        <div>    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-activity-heartbeat" width="400" height="350" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M3 12h4.5l1.5 -6l4 12l2 -9l1.5 3h4.5" /></svg>
-        <h1 class="text-6xl text-white font-bold">Vital Health</h1></div>
+<body class="w-full h-screen flex flex-col">
+    <div class="h-2/5">
+        @include('templates.header')
+        <div class="">
+            <object data="" type="image/svg+xml"></object>
+        </div>
     </div>
-    <div class="w-2/4 p-5">
-        <ul class="w-full h-16 flex justify-center items-center">
-            <li class="inline-block ml-16 text-2xl font-semibold cursor-pointer"><a href="/">Home</a></li>
-            <li class="inline-block ml-16 text-2xl font-semibold cursor-pointer"><a href="/login">Login</a></li>
-            <li class="inline-block ml-16 text-2xl font-semibold cursor-pointer"><a href="/register">Registro</a></li>
-            <li class="inline-block ml-16 text-2xl font-semibold cursor-pointer"><a href="/services">Servicios</a></li>
-        </ul>
-        <h1 class="text-black text-8xl uppercase italic font-bold mt-40">Esto es</h1>
-        <h1 class="text-black text-8xl uppercase italic font-bold">vital health</h1>
-        <h2 class="text-4xl">Tu salud es nuestro beneficio</h2>
-        <p class="mt-20 w-3/5 text-xl">
-        ¿Por qué es importante la salud? Existen varios beneficios de tener una vida saludable, pero el principal de ellos que podríamos nombrar es que nuestro cuerpo se libera de las diversas formas de trastornos y complicaciones y, por tanto, se obtiene una vida más larga, sin sufrir ningún tipo de dolores o malestares.
-        </p>
-        <p class="text-lg mt-56">&copy 2024 Vital Health. Todos los derechos reservados</p>
+    <div class="flex flex-col bg-vh-green text-white w-full h-3/5 px-6 py-4">
+        <h1 class="text-5xl font-semibold">VITAL</h1>
+        <h1 class="text-5xl font-semibold">HEALTH</h1>
+        <p class="text-1xl font-bold mb-4">Tus salud nuestro compromiso</p>
+        <p class="text-md mb-4">¿Por qué es importante la salud? Existen varios beneficios de tener una vida saludable, pero el principal de ellos que podríamos nombrar es que nuestro cuerpo se libera de las diversas formas de trastornos y complicaciones y, por tanto, se obtiene una vida más larga, sin sufrir ningún tipo de dolores o malestares.</p>
+        <a href="/login" target="_self" class="bg-white text-black p-2 font-bold rounded w-1/2 text-center">
+            Iniciar Sesión
+        </a>
+    </div>
     </div>
 </body>
 </html>
---
+++ vh/resources/views/templates/header.blade.php
@@ -0,0 +1,17 @@
+
+<header class="w-full h-5">
+    <div class="flex w-full h-auto p-2 justify-between">
+        <object data="" type="image/svg+xml" width="100" height="20"></object><!--Logo-->
+        <!--  -->
+        <div>
+        <object data="{{asset('storage/svg/menu.svg')}}" type="image/svg+xml" width="50" height="50" class="float-right"></object>
+        <ul class="text-left mt-4 hidden">
+            <li class="font-bold mb-4">Inicio</li>
+            <li class="font-bold mb-4">Inicio de sesión</li>
+            <li class="font-bold mb-4">Registro</li>
+            <li class="font-bold mb-4">Servicios</li>
+        </ul>
+        </div>
+        </div>
+</header>
+
