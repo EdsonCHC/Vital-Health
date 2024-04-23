3ef8241f3b637c9ff023d53494f588b0932ebc0c
Happy162006 <galdamezsoto13@gmail.com>
2024-04-22 23:16:20 -0600

    index

--- vh/resources/views/app/index.blade.php
+++ vh/resources/views/app/index.blade.php
@@ -24,15 +24,30 @@
             <div class="w-14 mb-2  bg-vh-green">a</div>
         </div>
     </div>
-    <div class="flex flex-col bg-vh-green text-white w-full h-3/5 px-6 py-4">
-        <h1 class="text-5xl font-semibold">VITAL</h1>
-        <h1 class="text-5xl font-semibold">HEALTH</h1>
-        <p class="text-1xl font-bold mb-4">Tus salud nuestro compromiso</p>
-        <p class="text-md mb-4">¿Por qué es importante la salud? Existen varios beneficios de tener una vida saludable, pero el principal de ellos que podríamos nombrar es que nuestro cuerpo se libera de las diversas formas de trastornos y complicaciones y, por tanto, se obtiene una vida más larga, sin sufrir ningún tipo de dolores o malestares.</p>
-        <a href="/login" target="_self" class="bg-white text-black p-2 font-bold rounded w-1/2 text-center">
-            Iniciar Sesión
+    <div class="flex justify-center items-center">
+    <div class="w-1/2">
+        <div class="ml-4 mt-4 mb-2">
+            <span class="font-bold text-lg">Brindamos Ayuda</span>
+            <span class="font-bold text-lg">En tu Futuro</span>
+        </div>
+        <a href="/login" target="_self" class="w-4/5 h-8 text-white bg-vh-green text-sm shadow-xl mb-10 rounded-full lg:w-2/5 hover:bg-white hover:text-vh-green inline-block text-center pt-1 mx-4">
+            Logueate ahora
         </a>
     </div>
+    <div class="w-1/2">
+        <p class="text-xs text-justify mb-4 font-bold text-gray-400 mx-4 -mt-1">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    </div>
+</div>
+<div class="flex justify-center text-center flex-col items-center">
+    <h2 class="font-bold text text-xl">Lorem Ipsum</h2>
+    <p class="text-sm font-bold text-gray-400 mx-4 mt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    <div class="mx-4 bg-green-100 py-4 my-4 rounded-lg shadow-lg px-8 ">
+          <h2 class="text-lg font-bold mb-2">Juan Jose Galdamez</h2>
+          <p class="text-sm font-bold text-gray-400 mt-2">Primary Care Doctor</p>
+          <a class="text-xs sm:text-sm text-justify text-normal block">Very personable and good listener.
+          Makes you feel comfortable and open immediately. Felt well taken care...</a>
+    </div>
+</div>
     <div class="h-2/5 hidden">
         @include('templates.footer')
         <div class="">
