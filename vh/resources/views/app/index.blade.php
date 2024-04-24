5ac41b18fecdf6b41190a8b2b15ad12bfbd66304
Happy162006 <galdamezsoto13@gmail.com>
2024-04-23 22:27:25 -0600

    Index:Cel-80%

--- vh/resources/views/app/index.blade.php
+++ vh/resources/views/app/index.blade.php
@@ -35,32 +35,152 @@
         </div>
     </div>
 <div class="flex justify-center text-center flex-col items-center">
-    <h2 class="font-bold text text-xl text-vh-green ">Lorem Ipsum</h2>
-    <p class="text-sm font-bold text-gray-400 mx-4 mt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-    <div class="w-3/4 mx-4 bg-blue-100 py-4 my-4 rounded-2xl shadow-lg px-8 ">
-          <h2 class="text-lg font-bold mb-2 text-vh-green">Juan Jose Galdamez</h2>
-          <p class="text-sm font-bold text-gray-400 mt-2">Primary Care Doctor</p>
-          <a class="text-md text-justify font-bold text-vh-green-light block leading-7">Very personable and good listener.
-          Makes you feel comfortable and open immediately. Felt well taken care...</a>
+<div>
+    <div>
+        <h2 class="font-bold text-xl text-vh-green">Lorem Ipsum</h2>
+        <p class="text-sm font-bold text-gray-400 mx-4 mt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
     </div>
-    <div class="w-3/4 mx-4 bg-blue-100 py-4 my-4 rounded-2xl shadow-lg px-8 ">
-          <h2 class="text-lg font-bold mb-2 text-vh-green">Juan Jose Galdamez</h2>
-          <p class="text-sm font-bold text-gray-400 mt-2">Primary Care Doctor</p>
-          <a class="text-md text-justify font-bold text-vh-green-light block leading-7">Very personable and good listener.
-          Makes you feel comfortable and open immediately. Felt well taken care...</a>
+    <div class="flex justify-center">
+        <div class="w-3/4 mx-4 bg-blue-100 py-4 my-4 rounded-2xl shadow-lg px-8">
+            <h2 class="text-lg font-bold mb-2 text-vh-green">Juan Jose Galdamez</h2>
+            <p class="text-sm font-bold text-gray-400 mt-2">Primary Care Doctor</p>
+            <a class="text-md text-justify text-vh-green block leading-7">Very personable and good listener. Makes you feel comfortable and open immediately. Felt well taken care...</a>
+        </div>
     </div>
-    <div class="w-3/4 mx-4 bg-blue-100 py-4 my-4 rounded-2xl shadow-lg px-8 ">
-          <h2 class="text-lg font-bold mb-2 text-vh-green">Juan Jose Galdamez</h2>
-          <p class="text-sm font-bold text-gray-400 mt-2">Primary Care Doctor</p>
-          <a class="text-md text-justify font-bold text-vh-green-light block leading-7">Very personable and good listener.
-          Makes you feel comfortable and open immediately. Felt well taken care...</a>
+    <div class="flex justify-center">
+        <div class="w-3/4 mx-4 bg-blue-100 py-4 my-4 rounded-2xl shadow-lg px-8">
+            <h2 class="text-lg font-bold mb-2 text-vh-green">Juan Jose Galdamez</h2>
+            <p class="text-sm font-bold text-gray-400 mt-2">Primary Care Doctor</p>
+            <a class="text-md text-justify text-vh-green block leading-7">Very personable and good listener. Makes you feel comfortable and open immediately. Felt well taken care...</a>
+        </div>
     </div>
 </div>
-    <div class="h-2/5 hidden">
-        @include('templates.footer')
+<div class="flex justify-center text-center flex-col items-center">
+    <div>
+        <div class="mb-2">
+            <h2 class="font-bold text-xl text-vh-green">Lorem Ipsum</h2>
+            <p class="text-sm font-bold text-gray-400 mx-4 mt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <a class="text-md my-1 text-vh-green-medium block leading-7">Very personable and good listener.</a>
+        </div>
+        <div class="flex justify-center">
+             <div class="flex flex-col w-full ">
+                 <div class="w-8/12 mx-auto bg-blue-100 py-20 my-4 rounded-2xl shadow-lg px-8">
+                 </div>
+                 <div class="w-8/12 mx-auto bg-blue-100 py-20  my-4 rounded-2xl shadow-lg px-8">
+                 </div>
+                 <div class="w-8/12 mx-auto bg-blue-100 py-20  my-4 rounded-2xl shadow-lg px-8">
+                 </div>
+            </div>
+         </div>
+   </div>
+</div>
+<div class="flex">
+    <div>
+        <div class="m-8">
+            <h2 class="font-bold text-left text-xl  text-vh-green">Las ventajas que posee con
+             nuestros servicios son:</h2>
+        </div>
+        <div class="flex justify-center">
+           <div class="w-9/12 mx-auto flex flex-wrap justify-between">
+                <!-- Primera columna -->
+                <div class="w-5/12 bg-gray-100 py-4 my-4 rounded-2xl shadow-lg">
+                   <p class="font-bold text-sm text-gray-400 mt-1">Primary Care</p>
+                </div>
+                <div class="w-5/12 bg-gray-100 py-4 my-4 rounded-2xl shadow-lg">
+                   <p class="font-bold text-sm text-gray-400 mt-1">Primary Care</p>
+                </div>
+                <div class="w-5/12 bg-gray-100 py-4 my-4 rounded-2xl shadow-lg">
+                   <p class="font-bold text-sm text-gray-400 mt-1">Primary Care</p>
+                </div>
+                    <!-- Segunda columna -->
+                <div class="w-5/12 bg-gray-100 py-4 my-4 rounded-2xl shadow-lg">
+                   <p class="font-bold text-sm text-gray-400 mt-1">Primary Care</p>
+                </div>
+                <div class="w-5/12 bg-gray-100 py-4 my-4 rounded-2xl shadow-lg">
+                   <p class="font-bold text-sm text-gray-400 mt-1">Primary Care</p>
+                </div>
+                <div class="w-5/12 bg-gray-100 py-4 my-4 rounded-2xl shadow-lg">
+                   <p class="font-bold text-sm text-gray-400 mt-1">Primary Care</p>
+                </div>
+           </div>
+        </div>
+   </div>
+</div>
+
+<div class=" mx-auto ">
+     <div class="w-11/12 mx-auto bg-vh-green py-11 my-4 ">
+     </div> 
+     <div class="flex justify-center text-center items-center flex-col">
+         <h2 class="font-bold text-xl text-vh-green">¿Por qué elegir nuestros servicios?</h2>
+             <div class="flex justify-center text-left items-center">
+                <div class="flex flex-col">
+                       <p class="text-xs font-bold text-gray-400 mx-6 mt-2">- Reach patients in your area looking for a new provider.</p>
+                       <p class="text-xs font-bold text-gray-400 mx-6 mt-2">- Reach patients in your area looking for a new provider.</p>
+                       <p class="text-xs font-bold text-gray-400 mx-6 mt-2">- Reach patients in your area looking for a new provider.</p>
+               </div>
+            </div>
+            <a href="/login" target="_self" class="w-2/5 h-8 text-white bg-vh-green text-sm shadow-xl m-5 rounded-full lg:w-2/5 hover:bg-white hover:text-vh-green inline-block text-center pt-1 mx-4">
+             Comienza ya             
+            </a>
+     </div>
+</div>
+<div class="w-11/12 mx-auto m-2 pt-5 pb-10 bg-blue-100 rounded-2xl shadow-lg px-8">
+    <h2 class="font-bold text-center text-xl  pb-8 text-vh-green">Servicios</h2>
+    <div class="flex justify-between">
+    <ul class="w-5/12 list-none pl-0">
+    <li class="text-vh-green relative pl-5 mb-2 ml-4">
+        <span class="absolute left-0 top-0 transform -translate-x-4">&#10148;</span>
+        Servicio 1
+    </li>
+    <li class="text-vh-green relative pl-5 mb-2 ml-4">
+        <span class="absolute left-0 top-0 transform -translate-x-4">&#10148;</span>
+        Servicio 2
+    </li>
+    <li class="text-vh-green relative pl-5 mb-2 ml-4">
+        <span class="absolute left-0 top-0 transform -translate-x-4">&#10148;</span>
+        Servicio 3
+    </li>
+    <li class="text-vh-green relative pl-5 mb-2 ml-4">
+        <span class="absolute left-0 top-0 transform -translate-x-4">&#10148;</span>
+        Servicio 4
+    </li>
+    
+</ul>
+<ul class="w-5/12 list-none pl-0">
+    <li class="text-vh-green relative pl-5 mb-2 ml-4">
+        <span class="absolute left-0 top-0 transform -translate-x-4">&#10148;</span>
+        Servicio 5
+    </li>
+    <li class="text-vh-green relative pl-5 mb-2 ml-4">
+        <span class="absolute left-0 top-0 transform -translate-x-4">&#10148;</span>
+        Servicio 6
+    </li>
+    <li class="text-vh-green relative pl-5 mb-2 ml-4">
+        <span class="absolute left-0 top-0 transform -translate-x-4">&#10148;</span>
+        Servicio 7
+    </li>
+    <li class="text-vh-green relative pl-5 mb-2 ml-4">
+        <span class="absolute left-0 top-0 transform -translate-x-4">&#10148;</span>
+        Servicio 8
+    </li>
+</ul>
+</div>
+</div>
+<div>
+ <h3 class=" text-left m-4  text-vh-green font-bold" >Vital Healt</h3>
+  <h2 class=" text-center  font-bold  mt-3 mx-9 text-xl lg:bg-black">Únase a nosotros y brindaremos una asistencia sanitaria de calidad. </h2>
+  <a href="/login" target="_self" class="w-2/5 h-8 text-white bg-vh-green text-sm shadow-xl mt-5 rounded-full lg:w-2/5 hover:bg-white hover:text-vh-green inline-block text-center pt-1 mx-4">
+             Unete ya             
+   </a>
+   <div class="w-11/12 mx-auto bg-vh-green py-11 my-5 ">
+    </div> 
+
+</div>
+<div class="h-2/5 hidden">
+        @include('templates.footer')  
         <div class="">
             <object data="" type="image/svg+xml"></object>
         </div>
-    </div>
+</div>
 </body>
 </html>
