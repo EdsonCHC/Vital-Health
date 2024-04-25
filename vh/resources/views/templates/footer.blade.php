91e6350a910a8c359de53e07510f16a2c9b7e2b4
Edson CHC <105886687+EdsonCHC@users.noreply.github.com>
2024-04-25 01:20:39 -0600

    refactor: index, desktop version 50%

--- vh/resources/views/templates/footer.blade.php
+++ vh/resources/views/templates/footer.blade.php
@@ -1,22 +1,5 @@
-<footer class="w-full h-16 rounded-t-lg bg-[#F5F5F5] absolute bottom-0 p-1 lg:bg-blue-100 hidden">
-    <div class="w-full flex items-center justify-around ">
-        <a href="" class="w-1/5 flex justify-center">
-            <img src="{{asset('storage/svg/hand.svg')}}" type="image/svg+xml"/>
-        </a>
-        <a href="" class="w-1/5 flex justify-center">
-            <img src="{{asset('storage/svg/user.svg')}}" type="image/svg+xml"/>
-        </a>
-        <a href="" class="w-1/5 flex justify-center">
-            <img class="p-4 bg-green-500 rounded-full" src="{{asset('storage/svg/home.svg')}}" type="image/svg+xml"/>
-        </a>
-        <a href="" class="w-1/5 flex justify-center">
-            <img src="{{asset('storage/svg/calendar.svg')}}" type="image/svg+xml">
-        </a>
-        <a href="" class="w-1/5 flex justify-center">
-            <img src="{{asset('storage/svg/chat.svg')}}" type="image/svg+xml"/>
-        </a>
-    </div>
-    <div class="hidden lg:flex w-full h-full items-center justify-center">
+<footer class="w-full h-16 rounded-t-lg bg-[#F5F5F5] p-1 lg:bg-blue-100">
+    <div class="bg-blue-100 flex w-full h-full items-center justify-center">
         <p>&copy 2024 Vital Health Todos los derechos reservados</p>
     </div>
 </footer>
