79c67378fbfecb3772eaccd1bc6bee112dbcc8a0
EdsonCHC <105886687+EdsonCHC@users.noreply.github.com>
2024-05-03 08:32:16 -0600

    refactor: add button in medicine

--- vh/resources/views/app/medicine.blade.php
+++ vh/resources/views/app/medicine.blade.php
@@ -15,14 +15,17 @@
         </button>
     </div>
     <div class="w-full h-auto">
-        <div id="medicine_container" class="w-full h-auto p-5 md:w-80 lg:w-80">
-            <div class="w-full h-auto bg-gray-300 rounded-lg p-2 flex gap-2 items-center">
-                <object data="{{asset('storage/svg/medicine_example.svg')}}" type="image/svg+xml"></object>
-                <div>
+        <div id="medicine_container" class="w-full h-auto p-5 md:w-72 lg:w-72">
+            <div class="w-full h-auto bg-gray-300 rounded-lg p-2 flex gap-2 items-center md:flex-col md:w-auto">
+                <object data="{{asset('storage/svg/medicine_example.svg')}}" type="image/svg+xml" class="w-full object-cover"></object>
+                <div class="w-full h-auto">
                     <p class="text-lg font-bold">Pedido #1</p>
                     <p>Medicamento</p>
-                    <p>Total: 3</p>
-                    <p class="text-blue-500">3h 10m 3s</p>
+                    <div class="w-full h-full flex flex-col md:flex-row lg:flex-row justify-between">
+                        <p>Total: 3</p>
+                        <p class="text-blue-500">3h 10m 3s</p>
+                    </div>
+                    <button class="w-auto h-auto bg-vh-green-light text-white p-1 rounded-2xl mx-auto my-0 md:w-full lg:w-full">Ver Pedido</button>
                 </div>
             </div>
         </div>
