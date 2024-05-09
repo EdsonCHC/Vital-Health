475fe604c6b7a138368e0c7aa6d6ce102b770c1c
Happy162006 <galdamezsoto13@gmail.com>
2024-05-08 21:04:19 -0600

    Perf:update area

--- vh/resources/views/app/area.blade.php
+++ vh/resources/views/app/area.blade.php
@@ -15,10 +15,11 @@
     </div>
     <div class="w-full ">
 
-        <div class=" px-44  pt-40 ">
-            <h2 class="font-bold text-xl lg:text-4xl  mb-6">
+        <div class=" hidden lg:flex  px-44  pt-40 ">
+            <h2 class="font-bold text-2xl lg:text-4xl  mb-6">
                 Servicios</h2>
         </div>
+
         <div class=" hidden lg:flex px-44  py-8 ">
             <div class="w-3/4">
                 <form method="get" action="#" class="relative z-50">
@@ -41,94 +42,51 @@
                 </div>
             </div>
         </div>
-        <div class="flex flex-wrap m-4 lg:m-16">
-            <div class="w-full lg:w-1/4 p-4">
-                <div class="bg-green-50 pt-5">
-                    <div class="  bg-green-100 rounded-full h-14 w-14 flex items-center justify-center ml-5">
-                        <h2 class="text-vh-green font-bold text-2xl">01</h2>
-                    </div>
-                    <h3 class="font-semibold text-xl p-2">Standardization</h3>
-                    <div class="text-container overflow-hidden">
-                        <p id="text1" class="text-base leading-loose p-4 line-clamp-3">
-                            Cuando trabajas en un entorno laboral global, a menudo es difícil evaluar las experiencias
-                            de
-                            capacitación de los estudiantes, las cuales son riencias de capacitación de los estudiantes,
-                            las
-                            cuales son riencias de capacitación de los estudiantes, las cuales son riencias de
-                            capacitación de
-                            los estudiantes, las cuales son
-                        </p>
-                        <a href="#" onclick="toggleText('text1')" class="text-vh-green hover:underline block p-4">Learn
-                            More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="w-full lg:w-1/4 p-4">
-                <div class="border pt-5">
-                    <div class="bg-green-100 rounded-full h-14 w-14 flex items-center justify-center ml-5">
 
-                        <h2 class="text-vh-green font-bold text-2xl">02</h2>
-                    </div>
-                    <h3 class="font-semibold text-xl p-2">Standardization</h3>
-                    <div class="text-container overflow-hidden">
-                        <p id="text2" class="text-base leading-loose p-4 line-clamp-3">
-                            Cuando trabajas en un entorno laboral global, a menudo es difícil evaluar las experiencias
-                            de
-                            capacitación de los estudiantes, las cuales son riencias de capacitación de los estudiantes,
-                            las
-                            cuales son riencias de capacitación de los estudiantes, las cuales son riencias de
-                            capacitación de
-                            los estudiantes, las cuales son
-                        </p>
-                        <a href="#" onclick="toggleText('text2')" class="text-vh-green hover:underline block p-4">Learn
-                            More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="w-full lg:w-1/4 p-4">
-                <div class="bg-green-50 pt-5">
-                    <div class="  bg-green-100 rounded-full h-14 w-14 flex items-center justify-center ml-5">
-                        <h2 class="text-vh-green font-bold text-2xl">03</h2>
-                    </div>
-                    <h3 class="font-semibold text-xl p-2">Standardization</h3>
-                    <div class="text-container overflow-hidden">
-                        <p id="text3" class="text-base leading-loose p-4 line-clamp-3">
-                            Cuando trabajas en un entorno laboral global, a menudo es difícil evaluar las experiencias
-                            de
-                            capacitación de los estudiantes, las cuales son riencias de capacitación de los estudiantes,
-                            las
-                            cuales son riencias de capacitación de los estudiantes, las cuales son riencias de
-                            capacitación de
-                            los estudiantes, las cuales son
-                        </p>
-                        <a href="#" onclick="toggleText('text3')" class="text-vh-green hover:underline block p-4">Learn
-                            More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="w-full lg:w-1/4 p-4">
-                <div class="border pt-5">
-                    <div class="bg-green-100 rounded-full h-14 w-14 flex items-center justify-center ml-5">
 
-                        <h2 class="text-vh-green font-bold text-2xl">04</h2>
+        <div class=" lg:hidden px-8  pt-28 ">
+            <div class="w-full">
+                <form method="get" action="#" class="relative z-50">
+                    <div class="relative ml-26">
+                        <input type="text" name="s" id="s"
+                            class="block w-full  pl-16 py-2 border border-gray-200 rounded-md leading-5 bg-gray-50 text-gray-800 placeholder-gray-400 focus:outline-none focus:bg-white focus:text-gray-900 sm:text-sm transition duration-150 ease-in-out"
+                            placeholder="Buscar chat">
+                        <span class="absolute inset-y-0 left-0 pl-3 flex items-center">
+                            <object data="{{asset('storage/svg/lupa.svg')}}" type="image/svg+xml"></object>
+                        </span>
                     </div>
-                    <h3 class="font-semibold text-xl p-2">Standardization</h3>
-                    <div class="text-container overflow-hidden">
-                        <p id="text4" class="text-base leading-loose p-4 line-clamp-3">
-                            Cuando trabajas en un entorno laboral global, a menudo es difícil evaluar las experiencias
-                            de
-                            capacitación de los estudiantes, las cuales son riencias de capacitación de los estudiantes,
-                            las
-                            cuales son riencias de capacitación de los estudiantes, las cuales son riencias de
-                            capacitación de
-                            los estudiantes, las cuales son
-                        </p>
-                        <a href="#" onclick="toggleText('text4')" class="text-vh-green hover:underline block p-4">Learn
-                            More</a>
+                </form>
+            </div>
+
+        </div>
+
+        <div class="lg:hidden w-full px-16 my-5 flex ">
+            <div class="w-1/2 flex items-center ">
+                <h2 class="font-bold text-2xl lg:text-4xl mb-6">Servicios</h2>
+            </div>
+            <div class="ml-4 w-1/2 flex justify-end">
+                <div class="w-40 border-2 border-green-900 rounded-full flex items-center justify-center p-2">
+                    <h2 class="text-center text-vh-green font-bold">Todos</h2>
+                    <div class="ml-3">
+                        <object data="{{asset('storage/svg/filtro.svg')}}" type="image/svg+xml"></object>
                     </div>
                 </div>
             </div>
         </div>
+
+
+
+        <div class="flex flex-wrap m-4 lg:m-16">
+            <div class="w-full lg:w-1/4 p-4">
+                <div class="bg-green-50 py-5  justify-center items-center">
+                    <div class="bg-green-200 rounded-full h-28 w-24 lg:h-40 lg:w-36"></div>
+                    <h3 class="font-bold text-xl p-2">Pediatria</h3>
+                    <p class="text-gray-400"> Disponible</p>
+                    <div class="bg-vh-green rounded-full h-16 w-16 lg:h-16 lg:w-16"></div>
+                </div>
+            </div>
+        </div>
+
 
     </div>
 
