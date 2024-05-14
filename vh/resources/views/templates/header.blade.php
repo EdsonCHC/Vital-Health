f3fe27df3a6682a453b89fcd38a78d9500678a49
EdsonCHC <105886687+EdsonCHC@users.noreply.github.com>
2024-05-14 14:20:29 -0600

    feat: new hamburguer menu in the header

--- vh/resources/views/templates/header.blade.php
+++ vh/resources/views/templates/header.blade.php
@@ -1,27 +1,35 @@
+@vite('resources/css/menu.css')
 <header class="w-full h-auto flex justify-start p-4">
-    <div class="w-full h-full">
-        <div class="w-full h-full mx-auto flex justify-between">
-            <object id="menuIcon" data="{{asset('storage/svg/menu.svg')}}" type="image/svg+xml" width="50" height="50" class="menu-button float-right p-2 bg-vh-green rounded-lg cursor-pointer lg:hidden"></object>
-            <ul class="hidden menu-list w-full mt-4 lg:flex justify-center gap-10">
-                <li class="font-bold mb-4"><a href="/">Inicio</a></li>
-                <li class="font-bold mb-4"><a href="/service">Servicios</a></li>
-                <li class="font-bold mb-4">Inicio de sesión</li>
-                <li class="font-bold mb-4">Registro</li>
+    <div class="w-full h-full mx-auto flex justify-between">
+        <section id="banner" class="sm:hidden">
+            <input type="checkbox" id="menu--toggle">
+            <label for="menu--toggle" id="trigger"></label>
+            <label for="menu--toggle" id="burger"></label>
+            <ul id="menu">
+                <li><a href="">Home</a></li>
+                <li><a href="">Produtos</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
             </ul>
-            <div class="hidden w-2/5 h-auto lg:flex lg:flex-row items-center justify-end gap-4">
-                <object data="{{asset('storage/svg/user.svg')}}" type="image/svg+xml"></object>
-                <div>
-                    <p class="font-bold">John Doe</p>
-                    <p>3rd year</p> <!--Esto para que?-->
-                </div>
-                <div class="">
-                    <a href="#"><img src="{{asset('storage/svg/noti.svg')}}" alt="noti_icon" class="w-8 p-1 bg-gray-300 rounded-full"></a>
-                    <a href="#"><img src="{{asset('storage/svg/config.svg')}}" alt="config_icon" class="w-8 p-1 bg-gray-300 rounded-full"></a>
-                </div>
+        </section>
+        <ul class="hidden menu-list w-full mt-4 lg:flex justify-center gap-10">
+            <li class="font-bold mb-4"><a href="/">Inicio</a></li>
+            <li class="font-bold mb-4"><a href="/service">Servicios</a></li>
+            <li class="font-bold mb-4">Inicio de sesión</li>
+            <li class="font-bold mb-4">Registro</li>
+        </ul>
+        <div class="hidden w-2/5 h-auto lg:flex lg:flex-row items-center justify-end gap-4">
+            <object data="{{asset('storage/svg/user.svg')}}" type="image/svg+xml"></object>
+            <div>
+                <p class="font-bold">John Doe</p>
+                <p>3rd year</p> <!--Esto para que?-->
             </div>
-        </div>
-
-        </div>      
+            <div class="flex flex-col gap">
+                <a href="#" class="m-0"><img src="{{asset('storage/svg/noti.svg')}}" alt="noti_icon"
+                        class="w-8 p-1 bg-gray-300 rounded-full"></a>
+                <a href="#" class="m-0"><img src="{{asset('storage/svg/config.svg')}}" alt="config_icon"
+                        class="w-8 p-1 bg-gray-300 rounded-full"></a>
+            </div>
         </div>
     </div>
 </header>
