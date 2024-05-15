2712f0e0638708ebfbea781d2970c6663c94a910
Happy162006 <105887488+Happy162006@users.noreply.github.com>
2024-05-15 14:25:15 -0600

    nav 50%

--- vh/resources/views/templates/headerdoc.blade.php
+++ vh/resources/views/templates/headerdoc.blade.php
@@ -1,46 +1,51 @@
-@vite('resources/css/menu.css')
 <div class="relative min-h-screen flex font-sans">
-
-    <div class="sidebar bg-gray-100 text-black w-64 space-y-6 py-7 px-2 relative">
+    <nav class="fixed top-0 left-0 bottom-0 bg-gray-100 text-black w-64 overflow-hidden z-10 flex flex-col">
         <!-- Logo -->
         <div class="flex items-center px-4">
             <img class="" src="{{asset('storage/svg/logo.svg')}}" type="image/svg+xml" />
         </div>
-        <!--  Nav -->
-        <nav class="flex flex-col">
-            <div>
+        <!-- Nav -->
+        <div class=" sidebar flex flex-col space-y-6 py-16 px-2 relative flex-grow">
+            <div class="flex flex-col space-y-4">
+                <div
+                    class="flex items-center justify-center block py-2.5 rounded transition duration-200 hover:bg-vh-green-medium hover:text-white text-black w-full">
+                    <div class="w-1/2 flex justify-end">
+                        <img class="h-8 w-8" src="{{asset('storage/svg/doctor.svg')}}" type="image/svg+xml" />
+                    </div>
+                    <div class="w-1/2 flex justify-start">
+                        <a href="#" class="">
+                            <span class="text-md font-bold">Inicio</span>
+                        </a>
+                    </div>
+                </div>
+
+
                 <a href="#"
                     class="flex items-center justify-center block py-2.5 rounded transition duration-200 hover:bg-vh-green-medium hover:text-white text-black">
-                    <span class="text-base">Inicio</span>
+                    <span class="text-md font-bold ">Servicio</span>
                 </a>
                 <a href="#"
                     class="flex items-center justify-center block py-2.5 rounded transition duration-200 hover:bg-vh-green-medium hover:text-white text-black">
-                    <span class="text-base">Servicio</span>
+                    <span class="text-md font-bold">Paciente</span>
                 </a>
                 <a href="#"
                     class="flex items-center justify-center block py-2.5 rounded transition duration-200 hover:bg-vh-green-medium hover:text-white text-black">
-                    <span class="text-base">Paciente</span>
-                </a>
-                <a href="#"
-                    class="flex items-center justify-center block py-2.5 rounded transition duration-200 hover:bg-vh-green-medium hover:text-white text-black">
-                    <span class="text-base">Citas</span>
+                    <span class="text-md font-bold">Citas</span>
                 </a>
                 <a href="#"
                     class="flex items-center justify-center block mb- py-2.5 rounded transition duration-200 hover:bg-vh-green-medium hover:text-white text-black">
-                    <span class="text-base">Programacion</span>
+                    <span class="text-md font-bold">Programacion</span>
                 </a>
-                <!--  logout-->
-
             </div>
-            <div class=" mt-40 "> 
+            <!-- Bottom Section -->
+            <div class="absolute bottom-0 left-0 right-0 mb-4 px-2">
                 <a href="#"
-                    class="flex items-center justify-center block py-2.5 rounded transition duration-200 hover:bg-vh-green hover:text-white text-black">
-                    <span class="text-base">Salir</span>
+                    class="flex items-center justify-center  block py-2.5 rounded transition duration-200 hover:bg-vh-green hover:text-white text-black">
+                    <span class="text-base font-semibold ">Salir</span>
                 </a>
-                <p class=" pt-24 text-center ">©2024 Vital Health.Todos los derechos reservados</p>
+                <p class="text-center text-base font-semibold px-2 py-2">©2024 Vital Health.Todos los derechos
+                    reservados</p>
             </div>
-
-        </nav>
-    </div>
-
+        </div>
+    </nav>
 </div>
