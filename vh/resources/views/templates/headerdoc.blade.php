0e03c978d6cdb34fa1b7b6ead9cc1d2786162e33
Happy162006 <galdamezsoto13@gmail.com>
2024-05-15 21:59:29 -0600

    Doctor view tools

--- vh/resources/views/templates/headerdoc.blade.php
+++ vh/resources/views/templates/headerdoc.blade.php
@@ -1,6 +1,6 @@
-<div class=" relative min-h-screen flex font-sans">
-    <nav class="hidden lg:flex fixed top-0 left-0 bottom-0 bg-gray-100 text-black w-64 overflow-hidden z-10 flex flex-col">
-        <!-- Logo -->
+<div class="relative min-h-screen flex font-sans">
+    <nav
+        class="hidden lg:flex fixed top-0 left-0 bottom-0 bg-gray-100 text-black w-64 overflow-hidden z-10 flex flex-col">
         <div class="flex items-center px-4">
             <img class="" src="{{asset('storage/svg/logo.svg')}}" type="image/svg+xml" />
         </div>
@@ -79,4 +79,22 @@
             </div>
         </div>
     </nav>
+
+    <nav class="lg:hidden relative flex items-stretch ">
+        <!--Hay se lo hechan xd-->
+
+    </nav>
+</div>
+<div class="hidden lg:flex lg:fixed lg:top-0 lg:right-0 lg:items-center lg:justify-end lg:gap-4 lg:p-4">
+    <object data="{{asset('storage/svg/doc.svg')}}" type="image/svg+xml"></object>
+    <div>
+        <p class="font-bold">John Doe</p>
+        <p class="font-semibold text-center">Doctor</p>
+    </div>
+    <div class="flex gap-2">
+        <a href="#" class="m-0"><img src="{{asset('storage/svg/config.svg')}}" alt="config_icon" class="w-8 p-1 "></a>
+    </div>
+</div>
+<div class="hidden lg:flex  fixed bottom-0 right-0 mb-4 mr-4">
+    <object data="{{asset('storage/svg/chat.svg')}}" type="image/svg+xml"></object>
 </div>
