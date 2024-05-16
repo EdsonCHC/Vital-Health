cd5c15f31791a03624060af5bea4c8d5e19266bc
Happy162006 <galdamezsoto13@gmail.com>
2024-05-15 21:27:05 -0600

    nav doctor pc 100%

--- vh/resources/views/templates/headerdoc.blade.php
+++ vh/resources/views/templates/headerdoc.blade.php
@@ -1,16 +1,16 @@
-<div class="relative min-h-screen flex font-sans">
-    <nav class="fixed top-0 left-0 bottom-0 bg-gray-100 text-black w-64 overflow-hidden z-10 flex flex-col">
+<div class=" relative min-h-screen flex font-sans">
+    <nav class="hidden lg:flex fixed top-0 left-0 bottom-0 bg-gray-100 text-black w-64 overflow-hidden z-10 flex flex-col">
         <!-- Logo -->
         <div class="flex items-center px-4">
             <img class="" src="{{asset('storage/svg/logo.svg')}}" type="image/svg+xml" />
         </div>
         <!-- Nav -->
-        <div class=" sidebar flex flex-col space-y-6 py-16 px-2 relative flex-grow">
+        <div class=" sidebar flex flex-col space-y-6 py-4 px-2 relative flex-grow">
             <div class="flex flex-col space-y-4">
                 <div
-                    class="flex items-center justify-center block py-2.5 rounded transition duration-200 hover:bg-vh-green-medium hover:text-white text-black w-full">
+                    class="flex items-center justify-center  py-2.5 rounded transition duration-200 hover:bg-vh-green-medium text-black w-full">
                     <div class="w-1/2 flex justify-end">
-                        <img class="h-8 w-8" src="{{asset('storage/svg/doctor.svg')}}" type="image/svg+xml" />
+                        <img class="h-8 w-8 mx-2" src="{{asset('storage/svg/homee.svg')}}" type="image/svg+xml" />
                     </div>
                     <div class="w-1/2 flex justify-start">
                         <a href="#" class="">
@@ -18,24 +18,55 @@
                         </a>
                     </div>
                 </div>
-
-
-                <a href="#"
-                    class="flex items-center justify-center block py-2.5 rounded transition duration-200 hover:bg-vh-green-medium hover:text-white text-black">
-                    <span class="text-md font-bold ">Servicio</span>
-                </a>
-                <a href="#"
-                    class="flex items-center justify-center block py-2.5 rounded transition duration-200 hover:bg-vh-green-medium hover:text-white text-black">
-                    <span class="text-md font-bold">Paciente</span>
-                </a>
-                <a href="#"
-                    class="flex items-center justify-center block py-2.5 rounded transition duration-200 hover:bg-vh-green-medium hover:text-white text-black">
-                    <span class="text-md font-bold">Citas</span>
-                </a>
-                <a href="#"
-                    class="flex items-center justify-center block mb- py-2.5 rounded transition duration-200 hover:bg-vh-green-medium hover:text-white text-black">
-                    <span class="text-md font-bold">Programacion</span>
-                </a>
+                <div
+                    class="flex items-center justify-center  py-2.5 rounded transition duration-200 hover:bg-vh-green-medium text-black w-full">
+                    <div class="w-1/2 flex justify-end">
+                        <img class="h-8 w-8 mx-2" src="{{asset('storage/svg/servicio.svg')}}" type="image/svg+xml" />
+                    </div>
+                    <div class="w-1/2 flex justify-start">
+                        <a href="#"
+                            class="flex items-center justify-center  py-2.5 rounded transition duration-200 hover:bg-vh-green-medium  text-black">
+                            <span class="text-md font-bold ">Servicio</span>
+                        </a>
+                    </div>
+                </div>
+                <div
+                    class="flex items-center justify-center  py-2.5 rounded transition duration-200 hover:bg-vh-green-medium text-black w-full">
+                    <div class="w-1/2 flex justify-end">
+                        <img class="h-8 w-8 mx-2" src="{{asset('storage/svg/paciente.svg')}}" type="image/svg+xml" />
+                    </div>
+                    <div class="w-1/2 flex justify-start">
+                        <a href="#"
+                            class="flex items-center justify-center block py-2.5 rounded transition duration-200 hover:bg-vh-green-medium  text-black">
+                            <span class="text-md font-bold">Paciente</span>
+                        </a>
+                    </div>
+                </div>
+                <div
+                    class="flex items-center justify-center  py-2.5 rounded transition duration-200 hover:bg-vh-green-medium text-black w-full">
+                    <div class="w-1/2 flex justify-end">
+                        <img class="h-8 w-8 mx-2" src="{{asset('storage/svg/cita.svg')}}" type="image/svg+xml" />
+                    </div>
+                    <div class="w-1/2 flex justify-start">
+                        <a href="#"
+                            class="flex items-center justify-center  py-2.5 rounded transition duration-200 hover:bg-vh-green-medium text-black">
+                            <span class="text-md font-bold">Citas</span>
+                        </a>
+                    </div>
+                </div>
+                <div
+                    class="flex items-center justify-center  py-2.5 rounded transition duration-200 hover:bg-vh-green-medium text-black w-full">
+                    <div class="w-1/2 flex justify-end">
+                        <img class="h-8 w-8 mx-2" src="{{asset('storage/svg/programacion.svg')}}"
+                            type="image/svg+xml" />
+                    </div>
+                    <div class="w-1/2 flex justify-start">
+                        <a href="#"
+                            class="flex items-center justify-center  mb- py-2.5 rounded transition duration-200 hover:bg-vh-green-medium  text-black">
+                            <span class="text-md font-bold">Programacion</span>
+                        </a>
+                    </div>
+                </div>
             </div>
             <!-- Bottom Section -->
             <div class="absolute bottom-0 left-0 right-0 mb-4 px-2">
