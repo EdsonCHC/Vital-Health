8a2828e6eb1f9d1231c381f7ec1d4329548dccf7
Happy162006 <galdamezsoto13@gmail.com>
2024-05-19 13:38:38 -0600

    Update:style medicine

--- vh/resources/views/app/medicine.blade.php
+++ vh/resources/views/app/medicine.blade.php
@@ -1,37 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Medicina</title>
     @vite('resources/css/app.css')
 </head>
-<body class="w-full h-screen">
+
+<body class="w-full h-screen ">
     @include('templates.header')
-    <div class="w-full h-auto flex flex-col items-start p-4 gap-2">
-        <p class="text-lg font-bold">Medicamentos Asignados</p>
+    <div class="w-full h-auto flex flex-col items-start p-4 gap-2 lg:px-16 ">
+        <h2 class="font-bold  text-xl text-vh-green lg:font-bold  lg:text-4xl">Medicamento Asignado </h2>
         <button class="text-sm font-bold">
             Solicitudes de medicamentos
         </button>
     </div>
-    <div class="w-full h-auto">
-        <div id="medicine_container" class="w-full h-auto p-5 md:w-72 lg:w-72">
-            <div class="w-full h-auto bg-gray-300 rounded-lg p-2 flex gap-2 items-center md:flex-col md:w-auto">
-                <object data="{{asset('storage/svg/medicine_example.svg')}}" type="image/svg+xml" class="w-full object-cover"></object>
-                <div class="w-full h-auto">
-                    <p class="text-lg font-bold">Pedido #1</p>
-                    <p>Medicamento</p>
-                    <div class="w-full h-full flex flex-col md:flex-row lg:flex-row justify-between">
-                        <p>Total: 3</p>
-                        <p class="text-blue-500">3h 10m 3s</p>
+    <div class="w-full h-auto  flex">
+        <div class=" bg-cover w-full flex justify-center items-center">
+            <div class="w-full bg-white lg:p-5 p-2 bg-opacity-40 backdrop-filter backdrop-blur-lg">
+                <div class="w-12/12 mx-auto rounded-2xl bg-white lg:p-5 p-2 bg-opacity-40 backdrop-filter backdrop-blur-lg">
+
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 text-center px-2 mx-auto">
+                        <article
+                            class="bg-white  lg:p-6 p-3 mb-6 shadow transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer border">
+                            <a target="_self" href=""
+                                class="absolute opacity-0 top-0 right-0 left-0 bottom-0"></a>
+                            <div class="relative mb-4 rounded-2xl">
+                                <object data="{{asset('storage/svg/medicine.svg')}}" type="image/svg+xml"
+                                    class="max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105"></object>
+                                <a class="flex justify-center items-center bg-green-700 bg-opacity-80 z-10 absolute top-0 left-0 w-full h-full text-white rounded-2xl opacity-0 transition-all duration-300 transform group-hover:scale-105 text-xl group-hover:opacity-100"
+                                    href="" target="_self" rel="noopener noreferrer">
+                                    Ver Medicamento
+                                    <svg class="ml-2 w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M13 5l7 7-7 7M5 5l7 7-7 7"></path>
+                                    </svg>
+                                </a>
+                            </div>
+                            <div class="flex justify-between items-center w-full pb-4 mb-auto">
+                                <div class="flex items-center">
+                                    <div class="pr-3">
+                                        <object class="h-10 w-10 rounded-full object-cover"
+                                            data="{{asset('storage/svg/medicine_example.svg')}}"
+                                            type="image/svg+xml"></object>
+
+                                    </div>
+                                    <div class="flex flex-1">
+                                        <div class="">
+                                            <p class="text-sm font-semibold ">Morris Muthigani</p>
+                                            <p class="text-sm text-gray-500">Published on 19/03/2024</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="flex justify-end">
+                                    <div class="text-sm flex items-center text-gray-500 ">
+                                        2 Days ago
+                                        <svg class="ml-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
+                                                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                            <h3 class="font-medium text-xl leading-8">
+                                <a href=""
+                                    class="block relative group-hover:text-vh-green transition-colors duration-200 ">
+                                    Instant Help at Your Fingertips
+                                </a>
+                            </h3>
+
+                        </article>
                     </div>
-                    <button class="w-auto h-auto bg-vh-green-light text-white p-1 rounded-2xl mx-auto my-0 md:w-full lg:w-full">Ver Pedido</button>
+
                 </div>
             </div>
+
         </div>
+
     </div>
-    <div class="w-full h-auto absolute bottom-0">
+    </div>
+
+    <div class="w-full h-auto bottom-0 absolute">
         @include('templates.footer')    
     </div>
 </body>
+
 </html>
