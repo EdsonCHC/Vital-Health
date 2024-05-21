986a2a8c8efe0f3dfaab8131f644bcf8154c2f1c
Edson CHC <105886687+EdsonCHC@users.noreply.github.com>
2024-05-21 00:14:41 -0600

    refactor(forms): added missing images

--- vh/resources/views/app/login.blade.php
+++ vh/resources/views/app/login.blade.php
@@ -1,40 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
-    <title>Inicio de sesión</title>
-    @vite(['resources/css/app.css','resources/css/swet.css','resources/js/app.js'])
-    <!-- CDNs -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
+  <title>Inicio de sesión</title>
+  @vite(['resources/css/app.css', 'resources/css/swet.css', 'resources/js/app.js'])
+  <!-- CDNs -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
 </head>
+
 <body class="w-full h-screen p-2 flex flex-col bg-white lg:flex-row overflow-hidden">
-    <div class="w-full h-screen flex flex-col items-center flex-1 lg:w-3/5">
-        <object data="{{asset('storage/svg/logo.svg')}}" type="image/svg+xml"></object><!--LOGO-->
-        <div id="form-div" class="grid place-items-center w-full">
-            <h1 class="text-2xl font-bold mb-5">Inicio de sesión</h1>
-            <form action="" class="text-center w-full flex flex-col items-center">
-                <Input  class="block h-10 p-2 outline outline-1 outline-vh-green w-4/5 mb-2 lg:w-2/5" type="text" placeholder="Nombre de Usuario o Email"></Input>
-                <input  class="block h-10 p-2 outline outline-1 outline-vh-green w-4/5 mb-10 lg:w-2/5" placeholder="Contraseña" type="password">
-                <button class="w-4/5 h-10 text-white bg-vh-green font-bold text-1xl shadow-xl mb-10 rounded-lg lg:w-2/5 hover:bg-white hover:text-vh-green">
-                Ingresar</button>
-            </form>
-        </div>
-        <p class="mb-8">¿No posees cuenta aun ? <a href="/registro" class="text-vh-green font-bold">Crear Cuenta</a></p>
-        <div class="flex gap-1 items-center">
-            <hr class="border border-vh-green opacity-25 w-14">
-            <p>Ingresa También con</p>
-            <hr class="border border-vh-green   opacity-25 w-14">
-        </div>
-        <div id="my-signin2"></div>
-        <p class="flex grow items-end text-vh-green text-center lg:grow-0 mt-8">&copy 2024 Vital Health Todos los derechos reservados</p>
+  <div class="w-full h-screen flex flex-col items-center flex-1 lg:w-3/5">
+    <object data="{{asset('storage/svg/logo.svg')}}" type="image/svg+xml"></object><!--LOGO-->
+    <div id="form-div" class="grid place-items-center w-full">
+      <h1 class="text-2xl font-bold mb-5">Inicio de sesión</h1>
+      <form action="" class="text-center w-full flex flex-col items-center">
+        <Input class="block h-10 p-2 outline outline-1 outline-vh-green w-4/5 mb-2 lg:w-2/5" type="text"
+          placeholder="Nombre de Usuario o Email"></Input>
+        <input class="block h-10 p-2 outline outline-1 outline-vh-green w-4/5 mb-10 lg:w-2/5" placeholder="Contraseña"
+          type="password">
+        <button
+          class="w-4/5 h-10 text-white bg-vh-green font-bold text-1xl shadow-xl mb-10 rounded-lg lg:w-2/5 hover:bg-white hover:text-vh-green">
+          Ingresar</button>
+      </form>
     </div>
-    <div class="hidden lg:block bg-vh-green w-2/5 rounded-xl p-11">
-        <div class="lg:bg-black opacity-20 w-full h-full rounded-xl"></div>1
+    <p class="mb-8">¿No posees cuenta aun ? <a href="/registro" class="text-vh-green font-bold">Crear Cuenta</a></p>
+    <div class="flex gap-1 items-center">
+      <hr class="border border-vh-green opacity-25 w-14">
+      <p>Ingresa También con</p>
+      <hr class="border border-vh-green   opacity-25 w-14">
     </div>
-    <!--COSO DE GOOGLE-->
-    <script> 
+    <div id="my-signin2"></div>
+    <p class="flex grow items-end text-vh-green text-center lg:grow-0 mt-8">&copy 2024 Vital Health Todos los derechos
+      reservados</p>
+  </div>
+  <div class="hidden lg:block bg-vh-green w-2/5 rounded-xl p-11">
+    <div class="lg:bg-black opacity-20 w-full h-full rounded-xl relative">
+    </div>
+    <div class="">
+      <img src="{{asset('storage/svg/n_doctor.svg')}}" alt="doctor_svg" class="absolute end-2 bottom-2">
+    </div>
+  </div>
+  <!--COSO DE GOOGLE-->
+  <script>
     function onSuccess(googleUser) {
       console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
     }
@@ -53,6 +63,7 @@
       });
     }
   </script>
-    <!-- <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script> API DE GOOGLE -->
-</body> 
+  <!-- <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script> API DE GOOGLE -->
+</body>
+
 </html>
--- vh/resources/views/app/registro.blade.php
+++ vh/resources/views/app/registro.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
     @vite(['resources/css/app.css', 'resources/css/sweet.css', 'resources/js/register.js'])
 </head>
+
 <body class="w-full h-screen">
     <div class="w-full h-screen p-2 flex flex-col bg-white lg:flex-row">
         <div class="flex flex-col w-full h-full lg:w-3/5 lg:order-2">
@@ -13,19 +15,30 @@
                 <object data="{{asset('storage/svg/logo.svg')}}" type="image/svg+xml"></object><!--LOGO-->
                 <h1 class="font-bold text-2xl">Registrarse</h1>
                 <form action="" class="w-4/5 lg:flex flex-col items-center">
-                    <input class="block w-full p-2 outline outline-1 outline-vh-green mb-4 lg:w-2/5 "type="text " placeholder="Nombres">
-                    <input class="block w-full p-2 outline outline-1 outline-vh-green mb-4 lg:w-2/5 "type="text" placeholder="Apellidos">
-                    <input class="block w-full p-2 outline outline-1 outline-vh-green mb-4 lg:w-2/5 "type="text" placeholder="Fecha de nacimiento">
-                    <input class="block w-full p-2 outline outline-1 outline-vh-green mb-4 lg:w-2/5 "type="email" placeholder="Email">
-                    <input class="block w-full p-2 outline outline-1 outline-vh-green mb-10 lg:w-2/5 "type="password" placeholder="Contraseña">
-                    <button id="register" class="w-full p-2 rounded-md bg-vh-green text-white font-bold  lg:w-2/5" id="show-alert">Registrarse</button>
+                    <input class="block w-full p-2 outline outline-1 outline-vh-green mb-4 lg:w-2/5 " type="text "
+                        placeholder="Nombres">
+                    <input class="block w-full p-2 outline outline-1 outline-vh-green mb-4 lg:w-2/5 " type="text"
+                        placeholder="Apellidos">
+                    <input class="block w-full p-2 outline outline-1 outline-vh-green mb-4 lg:w-2/5 " type="text"
+                        placeholder="Fecha de nacimiento">
+                    <input class="block w-full p-2 outline outline-1 outline-vh-green mb-4 lg:w-2/5 " type="email"
+                        placeholder="Email">
+                    <input class="block w-full p-2 outline outline-1 outline-vh-green mb-10 lg:w-2/5 " type="password"
+                        placeholder="Contraseña">
+                    <button id="register" class="w-full p-2 rounded-md bg-vh-green text-white font-bold  lg:w-2/5"
+                        id="show-alert">Registrarse</button>
                 </form>
             </div>
-            <p class="flex grow items-end text-vh-green text-center lg:block">© 2024 Vital Health Todos los derechos reservados</p>
+            <p class="flex grow items-end text-vh-green text-center lg:block">© 2024 Vital Health Todos los derechos
+                reservados</p>
         </div>
         <div class="hidden lg:block bg-vh-green w-2/5 rounded-xl p-11 order-1">
             <div class="lg:bg-black opacity-20 w-full h-full rounded-xl"></div>
+            <div class="">
+                <img src="{{asset('storage/svg/kid.svg')}}" alt="doctor_svg" class="absolute start-2 bottom-2">
+            </div>
         </div>
     </div>
 </body>
+
 </html>
