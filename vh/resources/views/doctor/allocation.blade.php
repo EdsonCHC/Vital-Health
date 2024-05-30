c4c19074ae03865487a1e266c0cc42b5f574bb95
Marvel983 <alejandro.josealvarenga@gmail.com>
2024-05-30 09:15:02 -0600

    Perf: Developed allocation

--- vh/resources/views/doctor/allocation.blade.php
+++ vh/resources/views/doctor/allocation.blade.php
@@ -19,6 +19,7 @@
             <div class=" my-4 mx-4   lg:mt-16 ">
                 <div class=" md:w-full w-full ">
                     <h2 class=" font-bold  text-2xl text-vh-green">Menu de Citas</h2>
+
                     <div class="w-full">
                         <form method="get" action="#" class="relative">
                             <div class="relative mt-5">
@@ -32,45 +33,46 @@
                         </form>
                     </div>
                 </div>
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 lg:mt-16 mt-4">
-                    <div class="bg-green-800 p-0.5 md:w-80  lg:w-auto rounded-xl">
-                        <div class="bg-green-200 p-4  rounded-xl">
-                            <h3 class="font-bold text-lg lg:text-2xl text-vh-green">Examenes de Sangre</h3>
-                            <a href="/login" target="_self"
-                                class="w-full h-10 lg:h-8 text-white bg-vh-green text-base lg:text-lg tracking-wide shadow-xl mt-4 lg:mt-8 mx-auto lg:mx-2 rounded-full hover:bg-white transition duration-300 hover:text-vh-green inline-block text-center content-center">
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:mt-16 mt-4">
+                <div class="bg-green-700 p-0.5 md:w-80  lg:w-full rounded-xl">
+                        <div class=" bg-green-160  p-4  rounded-xl">
+                            <div class="flex">
+                                <div class="w-autolg:flex  lg:m-1">
+                                    <h3 class="font-bold text-2xl lg:text-3xl text-white lg:w-auto">Examenes de Sangre</h3>
+                                </div>
+                                <div class="w-1/2 px-4 lg:px-2 ">
+                                    <img class=" lg:flex lg:justify-end lg:w-auto" src="{{asset('storage/svg/file.svg')}}" type="image/svg+xml" />
+                                </div>
+                               
+                            </div>
+                            <a href="/medicine_doc" target="_self"
+                                class="w-full h-10 lg:h-8  text-vh-green bg-white text-base lg:text-lg tracking-wide shadow-xl mt-4 lg:mt-8 mx-auto lg:mx-2 rounded-full hover:bg-white transition duration-300 hover:text-vh-green inline-block text-center content-center">
                                 Visualizar
                             </a>
                         </div>
+
                     </div>
-                    <div class="bg-green-800 p-0.5 md:w-80  lg:w-full rounded-xl">
-                        <div class="bg-green-200 p-4  rounded-xl">
-                            <h3 class="font-bold text-lg lg:text-2xl text-vh-green">Examenes de Sangre</h3>
-                            <a href="/login" target="_self"
-                                class="w-full h-10 lg:h-8 text-white bg-vh-green text-base lg:text-lg tracking-wide shadow-xl mt-4 lg:mt-8 mx-auto lg:mx-2 rounded-full hover:bg-white transition duration-300 hover:text-vh-green inline-block text-center content-center">
+                    <div class="bg-green-700 p-0.5 md:w-80  lg:w-full rounded-xl">
+                        <div class=" bg-green-160  p-4  rounded-xl">
+                            <div class="flex">
+                                <div class="w-autolg:flex  lg:m-1">
+                                    <h3 class="font-bold text-2xl lg:text-3xl text-white lg:w-auto">Examenes de Sangre</h3>
+                                </div>
+                                <div class="w-1/2 px-4 lg:px-2 ">
+                                    <img class=" lg:flex lg:justify-end lg:w-auto" src="{{asset('storage/svg/file.svg')}}" type="image/svg+xml" />
+                                </div>
+                               
+                            </div>
+                            <a href="/medicine_doc" target="_self"
+                                class="w-full h-10 lg:h-8  text-vh-green bg-white text-base lg:text-lg tracking-wide shadow-xl mt-4 lg:mt-8 mx-auto lg:mx-2 rounded-full hover:bg-white transition duration-300 hover:text-vh-green inline-block text-center content-center">
                                 Visualizar
                             </a>
                         </div>
+
                     </div>
-                    <div class="bg-green-800 p-0.5 md:w-80  lg:w-full rounded-xl">
-                        <div class="bg-green-200 p-4  rounded-xl">
-                            <h3 class="font-bold text-lg lg:text-2xl text-vh-green">Examenes de Sangre</h3>
-                            <a href="/login" target="_self"
-                                class="w-full h-10 lg:h-8 text-white bg-vh-green text-base lg:text-lg tracking-wide shadow-xl mt-4 lg:mt-8 mx-auto lg:mx-2 rounded-full hover:bg-white transition duration-300 hover:text-vh-green inline-block text-center content-center">
-                                Visualizar
-                            </a>
-                        </div>
-                    </div>
-                    <div class="bg-green-800 p-0.5 md:w-80  lg:w-full rounded-xl">
-                        <div class="bg-green-200 p-4  rounded-xl">
-                            <h3 class="font-bold text-lg lg:text-2xl text-vh-green">Examenes de Sangre</h3>
-                            <a href="/login" target="_self"
-                                class="w-full h-10 lg:h-8 text-white bg-vh-green text-base lg:text-lg tracking-wide shadow-xl mt-4 lg:mt-8 mx-auto lg:mx-2 rounded-full hover:bg-white transition duration-300 hover:text-vh-green inline-block text-center content-center">
-                                Visualizar
-                            </a>
-                        </div>
-                    </div>
-                   
-                 
+                
+
+
                 </div>
 
             </div>
