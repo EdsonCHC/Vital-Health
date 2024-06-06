b94d76d5abdc12769b8885a607fcf2e3c86e1676
Edson CHC <105886687+EdsonCHC@users.noreply.github.com>
2024-06-06 16:51:34 -0600

    test

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/loader.css",
+                "resources/js/preloader.js",
+                "resources/css/app.css",
+                "resources/js/animation.js",
+                "resources/css/animation.css",
+            ],
             refresh: true,
         }),
     ],
     resolve: {
         alias: {
-            '$': 'jQuery'
+            $: "jQuery",
         },
     },
 });
