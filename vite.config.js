f8daf6fc5aca3c06768bbd20fa7543f1033a32a4
Edson CHC <105886687+EdsonCHC@users.noreply.github.com>
2024-06-06 17:15:17 -0600

    refactor: vite for production

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,32 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                "resources/css/loader.css",
-                "resources/js/preloader.js",
+                //css
                 "resources/css/app.css",
-                "resources/js/animation.js",
+                "resources/css/loader.css",
+                "resources/css/menu.css",
+                "resources/css/sweet.css",
+                "resources/css/checkbox.css",
+                "resources/css/colorsChat.css",
+                "resources/css/chatsv1.css",
                 "resources/css/animation.css",
+                "resources/css/admin_staff.css",
+                //js
+                "resources/js/admin_appointment.js",
+                "resources/js/admin_edit_staff.js",
+                "resources/js/admin_history_staff.js",
+                "resources/js/admin_new_staff.js",
+                "resources/js/animation.js",
+                "resources/js/app.js",
+                "resources/js/appointment.js",
+                "resources/js/chatsv1.js",
+                "resources/js/citas.js",
+                "resources/js/login.js",
+                "resources/js/preloader.js",
+                "resources/js/register.js",
+                "resources/js/scroll.js",
+                "resources/js/stats_chart.js",
+                "resources/js/view.js"
             ],
             refresh: true,
         }),
