e30a99ddabca71e1b86c8083b46d2acbac76abad
Edson CHC <105886687+EdsonCHC@users.noreply.github.com>
2024-06-06 20:59:12 -0600

    feat: add a https method for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,10 +18,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url)
     {
-        if($this->app->environment('production')){
-            URL::forceScheme('https');
-        };
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceScheme('https');
+        }
     }
 }
