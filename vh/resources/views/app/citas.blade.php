040bf67b330a2b70e02f0c8d22e81c6b2c557038
Happy162006 <galdamezsoto13@gmail.com>
2024-06-09 18:05:03 -0600

    citas

--- vh/resources/views/app/citas.blade.php
+++ vh/resources/views/app/citas.blade.php
@@ -14,7 +14,8 @@
     <div class="w-full h-auto">
         @include('templates.header')
     </div>
-    <main class="w-full max-w-6xl h-auto p-4 lg:rounded-md mt-24 mx-auto bg-white sm:bg-blue-100">
+    <main class="w-full h-auto lg:px-10 p-4 lg:rounded-md lg:mt-10 mt-16 mx-auto bg-white ">
+
         <section class="flex justify-between">
             <h2 class="font-bold text-center text-2xl text-vh-green">Menu de Citas</h2>
             <div class="lg:relative inline-block text-left">
@@ -46,12 +47,39 @@
                 </div>
             </div>
         </section>
+
+        <script>
+            document.addEventListener("DOMContentLoaded", function () {
+                document.getElementById("menu-items").classList.add("hidden");
+            });
+
+            var menuButton = document.getElementById("menu-button");
+            var menuItems = document.getElementById("menu-items");
+
+            menuButton.addEventListener("click", function () {
+                var expanded = this.getAttribute("aria-expanded") === "true";
+                this.setAttribute("aria-expanded", !expanded);
+                menuItems.classList.toggle("hidden");
+            });
+
+            document.addEventListener("click", function (event) {
+                var isClickInsideMenu = menuButton.contains(event.target) || menuItems.contains(event.target);
+                if (!isClickInsideMenu) {
+                    menuItems.classList.add("hidden");
+                }
+            });
+        </script>
+
+
+
         <section class="w-full h-auto my-4 font-bold mt-12">
-        <div class="flex gap-8 flex-wrap justify-center lg:justify-start">
-                <div class="w-72 h-64 py-4 flex flex-col items-center bg-gray-100 rounded-lg relative sm:bg-white">
-                    <div class="w-full relative flex justify-center">
-                        <div class="absolute top-0 right-0 mt-2 mr-2">
-                            <div class="relative">
+
+            <div class="flex gap-8 flex-wrap justify-center lg:justify-start">
+
+                <div class="w-72 h-80 py-4 flex flex-col items-center bg-blue-50 rounded-lg ">
+                    <div class="flex justify-end w-full">
+                        <div class="lg:relative md:relative px-4">
+                            <div class="flex items-center justify-end">
                                 <button
                                     class="h-8 w-8 flex items-center justify-center bg-gray-300 rounded-full focus:outline-none dropdown-toggle">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600" fill="none"
@@ -60,8 +88,7 @@
                                             d="M19 9l-7 7-7-7"></path>
                                     </svg>
                                 </button>
-                                <div
-                                    class="hidden absolute right-0 mt-2 w-36 bg-white rounded-md shadow-lg dropdown-menu">
+                                <div class="hidden ml-2 mt-8 w-36 bg-white rounded-md shadow-lg dropdown-menu absolute">
                                     <div class="py-1">
                                         <button id="show-alert" href="#"
                                             class="block px-4 py-2 text-sm text-black hover:bg-gray-100 w-full">
@@ -78,8 +105,8 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="w-20 h-20 bg-purple-300 rounded-full"></div>
                     </div>
+                    <div class="w-20 h-20 bg-purple-300 rounded-full"></div>
                     <div class="w-9/12 h-28 p-2 text-center">
                         <h2>Odontología</h2>
                         <h3>Doc: Alejandro García</h3>
@@ -89,9 +116,142 @@
                         <h4>Fecha: 8 de junio de 2024</h4>
                     </div>
                 </div>
+
+
+                <!-- Second Container -->
+
+            </div>
+
+
+            <div class="flex justify-end w-full lg:my-16 py-4 px-4 ">
+
+                <div class="flex items-center justify-center mt-8">
+                    <div class="lg:relative">
+                        <button id="filterButton"
+                            class="px-4 py-2 bg-white border border-gray-300 text-gray-700 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400 transition duration-300 ease-in-out">
+                            Filtrar
+                            <span class="hidden md:inline">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline ml-1" viewBox="0 0 20 20"
+                                    fill="none" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M5 10l5-5 5 5m0 0l-5 5-5-5">
+                                    </path>
+                                </svg>
+                            </span>
+
+
+                        </button>
+                        <div id="filterMenu"
+                            class="absolute bottom-10 right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-md z-10 hidden">
+                            <a href="#"
+                                class="block px-4 py-2 text-gray-800 hover:bg-gray-100 transition duration-300 ease-in-out">Un
+                                dia
+                            </a>
+                            <a href="#"
+                                class="block px-4 py-2 text-gray-800 hover:bg-gray-100 transition duration-300 ease-in-out">Una
+                                semana
+                            </a>
+                            <a href="#"
+                                class="block px-4 py-2 text-gray-800 hover:bg-gray-100 transition duration-300 ease-in-out">Un
+                                mes
+                            </a>
+                        </div>
+                    </div>
+
+                    <script>
+                        const filterButton = document.getElementById('filterButton');
+                        const filterMenu = document.getElementById('filterMenu');
+
+                        filterButton.addEventListener('click', function () {
+                            filterMenu.classList.toggle('hidden');
+                        });
+
+                        document.addEventListener('click', function (event) {
+                            const isClickInside = filterButton.contains(event.target) || filterMenu.contains(event.target);
+                            if (!isClickInside) {
+                                filterMenu.classList.add('hidden');
+                            }
+                        });
+                    </script>
+
+                    <div class="lg:relative px-2 ">
+                        <button id="customMenuButton"
+                            class="px-4 py-2 bg-gray-200 text-gray-700 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline" viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path fill-rule="evenodd"
+                                    d="M4 12a2 2 0 110-4 2 2 0 010 4zm6 0a2 2 0 110-4 2 2 0 010 4zm6 0a2 2 0 110-4 2 2 0 010 4z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </button>
+                        <div id="customMenu"
+                            class="absolute top-10 right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-md z-10 hidden">
+                            <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-100">Elimiar historial </a>
+                            <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-100">Historial</a>
+                        </div>
+                    </div>
+
+                    <script>
+                        const customMenuButton = document.getElementById("customMenuButton");
+                        const customMenu = document.getElementById("customMenu");
+
+                        customMenuButton.addEventListener("click", function () {
+                            customMenu.classList.toggle("hidden");
+                        });
+
+                        document.addEventListener("click", function (event) {
+                            if (!customMenu.contains(event.target) && event.target !== customMenuButton) {
+                                customMenu.classList.add("hidden");
+                            }
+                        });
+                    </script>
+
+
+                    <button
+                        class="h-8 w-8 flex items-center justify-center bg-gray-300 rounded-full focus:outline-none ml-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7">
+                            </path>
+                        </svg>
+                    </button>
+
+                    <div class="ml-2 flex items-center justify-center w-8 h-8 bg-white rounded-md shadow-md">
+                        <span class="text-sm">1</span>
+                    </div>
+
+                    <button
+                        class="h-8 w-8 flex items-center justify-center bg-gray-300 rounded-full focus:outline-none ml-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7">
+                            </path>
+                        </svg>
+                    </button>
+                </div>
+
+
         </section>
+
+
+        <script>
+            document.addEventListener("click", function (event) {
+                const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+                dropdownToggles.forEach(function (toggle) {
+                    const dropdownMenu = toggle.nextElementSibling;
+                    if (toggle.contains(event.target) && dropdownMenu.classList.contains('hidden')) {
+                        dropdownMenu.classList.remove('hidden');
+                    } else {
+                        dropdownMenu.classList.add('hidden');
+                    }
+                });
+            });
+        </script>
+
+
     </main>
-    <div class="w-full h-auto lg:bottom-0 lg:absolute">
+
+    <div class="w-full h-auto absolute  lg:bottom-0">
         @include('templates.footer')
     </div>
 </body>
