908df137c30b3faf3bdc3344a833b78047e2cc0a
Marvel983 <alejandro.josealvarenga@gmail.com>
2024-07-16 10:04:57 -0600

    Footer styles

--- resources/views/templates/footer_two.blade.php
+++ resources/views/templates/footer_two.blade.php
@@ -1,38 +1,61 @@
-<footer class="flex flex-wrap mt-auto px-[60px] py-[2%] bg-cyan-100">
-    <div class="w-full md:w-1/4 sm:w-1/2 mb-[30px] text-center">
-        <h4 class="relative mb-8 text-2xl capitalize text-cyan-500">Atención al Cliente</h4>
-        <ul class="list-none">
-            <li>
-                <a href="" class="block capitalize text-xl text-vh-green-medium no-underline hover:text-vh-green-light">Medicina</a>
-                <a href="" class="block capitalize text-xl text-vh-green-medium no-underline hover:text-vh-green-light">Citas</a>
-                <a href="" class="block capitalize text-xl text-vh-green-medium no-underline hover:text-vh-green-light">Exámenes</a>
-                <a href="" class="block capitalize text-xl text-vh-green-medium no-underline hover:text-vh-green-light">Control personal</a>
-            </li>
-        </ul>
+<footer class="flex flex-wrap mt-auto px-[60px] py-[2%] bg-gray-300">
+    <div class="w-full flex justify-around items-center ">
+        <div class="w-full sm:w-1/2 md:w-1/4 mb-6 text-center">
+            <h4 class="mb-4 text-2xl font-bold capitalize ">Atención al Cliente</h4>
+            <ul class="list-none space-y-2">
+                <li><a href="#"
+                        class="block text-xl font-semibold capitalize text-vh-green-medium-2 no-underline hover:text-vh-green-medium-2">Medicina</a>
+                </li>
+                <li><a href="#"
+                        class="block text-xl font-semibold capitalize text-vh-green-medium-2 no-underline hover:text-vh-green-light">Citas</a>
+                </li>
+                <li><a href="#"
+                        class="block text-xl font-semibold capitalize text-vh-green-medium-2 no-underline hover:text-vh-green-light">Exámenes</a>
+                </li>
+                <li><a href="#"
+                        class="block text-xl font-semibold capitalize text-vh-green-medium-2 no-underline hover:text-vh-green-light">Control
+                        personal</a></li>
+            </ul>
+        </div>
+        <div class="w-full sm:w-1/2 md:w-1/4 mb-6 text-center">
+            <h4 class="mb-4 text-2xl font-bold capitalize ">Redes Sociales</h4>
+            <ul class="list-none space-y-2">
+                <li><a href="#"
+                        class="block text-xl font-semibold capitalize text-vh-green-medium-2 no-underline hover:text-vh-green-medium-2">Medicina</a>
+                </li>
+                <li><a href="#"
+                        class="block text-xl font-semibold capitalize text-vh-green-medium-2 no-underline hover:text-vh-green-light">Citas</a>
+                </li>
+                <li><a href="#"
+                        class="block text-xl font-semibold capitalize text-vh-green-medium-2 no-underline hover:text-vh-green-light">Exámenes</a>
+                </li>
+                <li><a href="#"
+                        class="block text-xl font-semibold capitalize text-vh-green-medium-2 no-underline hover:text-vh-green-light">Control
+                        personal</a></li>
+            </ul>
+        </div>
+        <div class="w-full sm:w-1/2 md:w-1/4 mb-6 text-center">
+            <h4 class="mb-4 text-2xl font-bold capitalize ">Atención al Cliente</h4>
+            <ul class="list-none space-y-2">
+                <li><a href="#"
+                        class="block text-xl font-semibold capitalize text-vh-green-medium-2 no-underline hover:text-vh-green-medium-2">Medicina</a>
+                </li>
+                <li><a href="#"
+                        class="block text-xl font-semibold capitalize text-vh-green-medium-2 no-underline hover:text-vh-green-light">Citas</a>
+                </li>
+                <li><a href="#"
+                        class="block text-xl font-semibold capitalize text-vh-green-medium-2 no-underline hover:text-vh-green-light">Exámenes</a>
+                </li>
+                <li><a href="#"
+                        class="block text-xl font-semibold capitalize text-vh-green-medium-2 no-underline hover:text-vh-green-light">Control
+                        personal</a></li>
+            </ul>
+        </div>
+        <div class="w-full md:w-1/4 sm:w-1/2 md:mb-[30px] text-center">
+            <object data="storage/svg/logo.svg" type="image/svg+xml"></object>
+        </div>
     </div>
-    <div class="w-full md:w-1/4 sm:w-1/2 mb-[30px] text-center">
-        <h4 class="relative mb-8 text-2xl capitalize text-cyan-500">Servicios</h4>
-        <ul class="list-none">
-            <li>
-                <a href="" class="block capitalize text-xl text-vh-green-medium no-underline hover:text-vh-green-light">Registrarse</a>
-                <a href="" class="block capitalize text-xl text-vh-green-medium no-underline hover:text-vh-green-light">Iniciar Sesión</a>
-                <a href="" class="block capitalize text-xl text-vh-green-medium no-underline hover:text-vh-green-light">Lorem, ipsum dolor.</a>
-                <a href="" class="block capitalize text-xl text-vh-green-medium no-underline hover:text-vh-green-light">Lorem, ipsum dolor.</a>
-            </li>
-        </ul>
-    </div>
-    <div class="w-full md:w-1/4 sm:w-1/2 md:mb-[30px] text-center">
-        <h4 class="relative mb-8 text-2xl capitalize text-cyan-500">Lorem</h4>
-        <ul class="list-none">
-            <li>
-                <a href="" class="block capitalize text-xl text-vh-green-medium no-underline hover:text-vh-green-light">Lorem, ipsum dolor.</a>
-                <a href="" class="block capitalize text-xl text-vh-green-medium no-underline hover:text-vh-green-light">Lorem, ipsum dolor.</a>
-                <a href="" class="block capitalize text-xl text-vh-green-medium no-underline hover:text-vh-green-light">Lorem, ipsum dolor.</a>
-            </li>
-        </ul>
-    </div>
-    <div class="w-full md:w-1/4 sm:w-1/2 md:mb-[30px] text-center">
-        <object data="storage/svg/logo.svg" type="image/svg+xml"></object>
-        <p>&copy Todos derechos reservados 2024 Vital Health</p>
+    <div class="mx-auto">
+        <p class="font-bold text-lg">&copy Todos derechos reservados 2024 Vital Health</p>
     </div>
 </footer>
