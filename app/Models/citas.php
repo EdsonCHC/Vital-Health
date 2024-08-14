20671ff41383c3c08d24f998f3553a6c424106c6
Happy162006 <105887488+Happy162006@users.noreply.github.com>
2024-08-14 15:56:36 -0600

    cita,en la cas sigo

--- app/Models/citas.php
+++ app/Models/citas.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class citas extends Model
+class Citas extends Model
 {
     use HasFactory;
 
@@ -14,19 +14,25 @@ class citas extends Model
         'date',
         'hour',
         'description',
-        'id_patient',
-        'id_doctor',
         'modo',
         'enlace',
+        'id_patient',
+        'id_category',
+        'id_doctor',
     ];
 
     public function patient()
     {
-        return $this->belongsTo('App\Models\patients', 'id_patient');
+        return $this->belongsTo('App\Models\Usuario', 'id_patient');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Categoria', 'id_category');
     }
 
     public function doctor()
     {
-        return $this->belongsTo('App\Models\doctors', 'id_doctor');
+        return $this->belongsTo('App\Models\Doctor', 'id_doctor'); 
     }
 }
--- database/migrations/2024_01_09_citas.php
+++ database/migrations/2024_01_09_citas.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     /**
-     * Run the migrations.
+     * Ejecutar las migraciones.
      */
     public function up(): void
     {
@@ -15,21 +15,23 @@ return new class extends Migration {
             $table->string('state')->default('1'); 
             $table->date('date');
             $table->time('hour');
-            $table->string('description', 1000);
+            $table->string('description', 1000)->nullable(); 
             $table->string('modo');
             $table->string('enlace')->nullable(); 
             $table->timestamp('published_at')->nullable()->index();
             $table->timestamps();
             $table->unsignedBigInteger('id_patient');
+            $table->unsignedBigInteger('id_category');
             $table->unsignedBigInteger('id_doctor')->nullable();
 
-            $table->foreign('id_patient')->references('id')->on('patients')->onDelete('cascade'); 
+            $table->foreign('id_patient')->references('id')->on('patients')->onDelete('cascade');
+            $table->foreign('id_category')->references('id')->on('categorias')->onDelete('cascade');
             $table->foreign('id_doctor')->references('id')->on('doctors')->onDelete('set null');
         });
     }
 
     /**
-     * Reverse the migrations.
+     * Revertir las migraciones.
      */
     public function down(): void
     {
