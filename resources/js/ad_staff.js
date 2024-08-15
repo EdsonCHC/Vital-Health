3caff296fc4441160e662cf3e8356f90bb9ab601
Edson CHC <105886687+EdsonCHC@users.noreply.github.com>
2024-08-15 00:25:14 -0600

    8u8nhohfsfdXDX:cndifpalskdys2

--- resources/js/ad_staff.js
+++ resources/js/ad_staff.js
@@ -5,7 +5,6 @@ window.$ = jQuery;
 // Editar info del Doc
 $(document).ready(function () {
     const url = new URL(window.location.href);
-
     const pathSegments = url.pathname.split("/");
     const id = pathSegments[pathSegments.length - 1];
 
@@ -36,11 +35,7 @@ $(document).ready(function () {
                         <input id="age" name="age" class="w-full h-10 my-2 px-2 border border-solid rounded-sm" type="number" min="18" max="100">
                     </div>
                     <div class="flex flex-col">
-                        <label class="mr-auto font-semibold text-xl">Dirección</label>
-                        <input id="address" name="address" class="w-full h-10 my-2 px-2 border border-solid rounded-sm" type="text">
-                    </div>
-                    <div class="flex flex-col">
-                        <label class="mr-auto font-semibold text-xl">Descripcion</label>
+                        <label class="mr-auto font-semibold text-xl">Descripción</label>
                         <input id="description" name="description" class="w-full h-10 my-2 px-2 border border-solid rounded-sm" type="text">
                     </div>
                 </form>
@@ -59,83 +54,51 @@ $(document).ready(function () {
                 </form>
             `;
 
-    $("#edit_staff").click((e) => {
-        e.preventDefault();
-        secuencia();
-    });
+    const firstFormEdit = `
+            <form id="firstForm" class="space-y-4" autocomplete="off">
+                <div class="flex flex-col">
+                    <label class="mr-auto font-semibold text-xl">Nombre</label>
+                    <input id="name" name="name" class="w-full h-10 my-2 px-2 border border-solid rounded-sm" type="text">
+                </div>
+                <div class="flex flex-col">
+                    <label class="mr-auto font-semibold text-xl">Apellidos</label>
+                    <input id="lastName" name="lastName" class="w-full h-10 my-2 px-2 border border-solid rounded-sm" type="text">
+                </div>
+                <div class="flex flex-col">
+                    <label class="mr-auto font-semibold text-xl">Número Telefónico</label>
+                    <input id="phone" name="phone" class="w-full h-10 my-2 px-2 border border-solid rounded-sm" type="text">
+                </div>
+                <div class="flex flex-col">
+                    <label class="mr-auto font-semibold text-xl">Género</label>
+                    <select id="gender" name="gender" class="w-full h-10 my-2 px-2 border border-solid rounded-sm">
+                        <option value="" disabled selected>Seleccione un género</option>
+                        <option value="Masculino">Masculino</option>
+                        <option value="Femenino">Femenino</option>
+                    </select>
+                </div>
+                <div class="flex flex-col">
+                    <label class="mr-auto font-semibold text-xl">Edad</label>
+                    <input id="age" name="age" class="w-full h-10 my-2 px-2 border border-solid rounded-sm" type="number" min="18" max="100">
+                </div>
+                <div class="flex flex-col">
+                    <label class="mr-auto font-semibold text-xl">Descripción</label>
+                    <input id="description" name="description" class="w-full h-10 my-2 px-2 border border-solid rounded-sm" type="text">
+                </div>
+            </form>
+        `;
 
-    async function showFirstForm() {
-        const { value: formValues, isConfirmed } = await Swal.fire({
-            title: "Datos del Doctor",
-            html: firstForm,
-            showConfirmButton: true,
-            confirmButtonText: "Siguiente",
-            confirmButtonColor: "#166534",
-            showCancelButton: true,
-            cancelButtonText: "Cancelar",
-            preConfirm: () => {
-                const form = Swal.getPopup().querySelector("#firstForm");
-                const name = form.name.value.trim();
-                const lastName = form.lastName.value.trim();
-                const phone = form.phone.value.trim();
-                const gender = form.gender.value;
-                const age = form.age.value;
-                const address = form.address.value.trim();
-                const description = form.description.value.trim();
-                if (
-                    !name ||
-                    !lastName ||
-                    !phone ||
-                    !gender ||
-                    !age ||
-                    !address ||
-                    !description
-                ) {
-                    Swal.showValidationMessage(
-                        "Por favor, complete todos los campos"
-                    );
-                    return false;
-                }
-                return {
-                    name,
-                    lastName,
-                    phone,
-                    gender,
-                    age,
-                    address,
-                    description,
-                };
-            },
-        });
-
-        return { formValues, isConfirmed };
-    }
-
-    async function showSecondForm() {
-        const { value: formValues, isConfirmed } = await Swal.fire({
-            title: "Credenciales del Doctor",
-            html: secondForm,
-            showConfirmButton: true,
-            confirmButtonText: "Crear",
-            confirmButtonColor: "#166534",
-            showCancelButton: true,
-            cancelButtonText: "Cancelar",
-            preConfirm: () => {
-                const form = Swal.getPopup().querySelector("#secondForm");
-                const email = form.email.value.trim();
-                const password = form.password.value.trim();
-                if (!email || !password) {
-                    Swal.showValidationMessage(
-                        "Por favor, complete todos los campos"
-                    );
-                    return false;
-                }
-                return { email, password };
-            },
-        });
-
-        return { formValues, isConfirmed };
-    }
+    const secondFormEdit = `
+            <form id="secondForm" class="space-y-4" autocomplete="off">
+                <div class="flex flex-col">
+                    <label class="mr-auto font-semibold text-xl">Correo</label>
+                    <input id="email" name="email" class="w-full h-10 my-2 px-2 border border-solid rounded-sm" type="email">
+                </div>
+                <div class="flex flex-col">
+                    <label class="mr-auto font-semibold text-xl">Contraseña</label>
+                    <input id="password" name="password" class="w-full h-10 my-2 px-2 border border-solid rounded-sm" type="password">
+                </div>
+            </form>
+        `;
 
     // Crear datos del Doc
     $("#create_staff").click((e) => {
@@ -159,7 +122,6 @@ $(document).ready(function () {
                 const phone = form.phone.value.trim();
                 const gender = form.gender.value;
                 const age = form.age.value;
-                const address = form.address.value.trim();
                 const description = form.description.value.trim();
                 if (
                     !name ||
@@ -167,7 +129,6 @@ $(document).ready(function () {
                     !phone ||
                     !gender ||
                     !age ||
-                    !address ||
                     !description
                 ) {
                     Swal.showValidationMessage(
@@ -181,7 +142,6 @@ $(document).ready(function () {
                     phone,
                     gender,
                     age,
-                    address,
                     description,
                 };
             },
@@ -194,6 +154,81 @@ $(document).ready(function () {
         const { value: formValues, isConfirmed } = await Swal.fire({
             title: "Credenciales del Nuevo Doctor",
             html: secondForm,
+            showConfirmButton: true,
+            confirmButtonText: "Crear",
+            confirmButtonColor: "#166534",
+            showCancelButton: true,
+            cancelButtonText: "Cancelar",
+            preConfirm: () => {
+                const form = Swal.getPopup().querySelector("#secondForm");
+                const email = form.email.value.trim();
+                const password = form.password.value.trim();
+                if (!email || !password) {
+                    Swal.showValidationMessage(
+                        "Por favor, complete todos los campos"
+                    );
+                    return false;
+                }
+                return { email, password };
+            },
+        });
+
+        return { formValues, isConfirmed };
+    }
+
+    $("#edit_staff").click((e) => {
+        e.preventDefault();
+        secuencia(doctorId);
+    });
+
+    async function showFirstForm(doctorId) {
+        const { value: formValues, isConfirmed } = await Swal.fire({
+            title: "Datos del Doctor",
+            html: firstFormEdit,
+            showConfirmButton: true,
+            confirmButtonText: "Siguiente",
+            confirmButtonColor: "#166534",
+            showCancelButton: true,
+            cancelButtonText: "Cancelar",
+            preConfirm: () => {
+                const form = Swal.getPopup().querySelector("#firstForm");
+                const name = form.name.value.trim();
+                const lastName = form.lastName.value.trim();
+                const phone = form.phone.value.trim();
+                const gender = form.gender.value;
+                const age = form.age.value;
+                const description = form.description.value.trim();
+                if (
+                    !name ||
+                    !lastName ||
+                    !phone ||
+                    !gender ||
+                    !age ||
+                    !description
+                ) {
+                    Swal.showValidationMessage(
+                        "Por favor, complete todos los campos"
+                    );
+                    return false;
+                }
+                return {
+                    name,
+                    lastName,
+                    phone,
+                    gender,
+                    age,
+                    description,
+                };
+            },
+        });
+
+        return { formValues, isConfirmed };
+    }
+
+    async function showSecondForm(doctorInfo) {
+        const { value: formValues, isConfirmed } = await Swal.fire({
+            title: "Credenciales del Doctor",
+            html: secondFormEdit,
             showConfirmButton: true,
             confirmButtonText: "Crear",
             confirmButtonColor: "#166534",
@@ -249,11 +284,45 @@ $(document).ready(function () {
                 });
             }
         }
-    }
-});
+    } 
 
-// Ver historial de pacientes del Doc
-$(document).ready(function () {
+    async function secuencia(doctorId) {
+        const { formValues: firstFormValues, isConfirmed: firstFormConfirmed } =
+            await showFirstForm(doctorId);
+        if (firstFormConfirmed) {
+            const {
+                formValues: secondFormValues,
+                isConfirmed: secondFormConfirmed,
+            } = await showSecondForm(doctorInfo);
+            if (secondFormConfirmed) {
+                const concatArrays = {
+                    ...firstFormValues,
+                    ...secondFormValues,
+                    category_id: id,
+                };
+                console.log(concatArrays);
+                $.ajax({
+                    url: "/staff/{id}",
+                    type: "POST",
+                    headers: {
+                        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
+                            "content"
+                        ), // Incluye el token CSRF
+                    },
+                    data: concatArrays,
+                    success: (response) => {
+                        console.log("Datos enviados", response);
+                    },
+                    error: (response) => {
+                        console.log("error al enviar los Datos", response);
+                    },
+                });
+            }
+        }
+    }
+
+    // Ver historial de pacientes del Doc
+
     $("#history_staff").click(async (e) => {
         e.preventDefault();
         await showTableAlert();
@@ -339,10 +408,9 @@ $(document).ready(function () {
             });
         });
     }
-});
 
-// Confirmar la eliminación del Doc
-$(document).ready(function () {
+    // Confirmar la eliminación del Doc
+
     $("#delete_doc").click((e) => {
         e.preventDefault();
         try {
